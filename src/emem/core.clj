78860e78d9e86245475a794f00bea4cd99898aae
Rommel M. Martinez <ebzzry@gmail.com>
2015-07-31 07:31:00 +0800

    Added --version.

--- src/emem/core.clj
+++ src/emem/core.clj
@@ -26,17 +26,28 @@
     :default 0
     ;; Use assoc-fn to create non-idempotent options
     :assoc-fn (fn [m k _] (update-in m [k] inc))]
+   ["-V" "--version" "display program version"]
    ["-h" "--help" "display this help"]])
 
-(defn usage
+;; (defn usage
+;;   "Displays program usage."
+;;   [summary]
+;;   (->> ["Usage: emem [OPTION]... [MARKDOWN_FILE]..."
+;;         ""
+;;         "Options:"
+;;         summary
+;;         ]
+;;        (s/join \newline)))
+
+(defn display-usage
   "Displays program usage."
   [summary]
-  (->> ["Usage: emem [OPTION]... [MARKDOWN_FILE]..."
-        ""
-        "Options:"
-        summary
-        ]
-       (s/join \newline)))
+  (-> (->> ["Usage: emem [OPTION]... [MARKDOWN_FILE]..."
+            ""
+            "Options:"
+            summary]
+           (s/join \newline))
+      println))
 
 (defn error-msg
   "Displays the errors encountered during command parsing."
@@ -44,44 +55,61 @@
   (str "The following errors occurred while parsing your command:\n"
        (s/join \newline errors)))
 
-(defn exit
-  "Exits the program with status code and message."
-  [status msg]
-  (println msg)
-  (System/exit status))
+(defn doexit
+  ""
+  [f & [code]]
+  (f)
+  (System/exit (or code 0)))
 
-(defn doc-title
-  "Returns the title of the document."
-  [file]
-  (with-open [file (io/reader file)]
-    (str (first (line-seq file)))))
+(defn msgexit
+  "Exits the program with status code and message."
+  [msg code]
+  (doexit #(println msg) code))
 
 (defn msg
   "Displays messages controlled by the verbosity option."
-  [text level req]
+  [text req & [level]]
   (when (>= (or level 0) req)
     (println text)))
 
+(defn version
+  "Prints program version."
+  []
+  (msg "Printing program version ..." 1)
+  (with-open [in (io/input-stream (io/resource "VERSION"))]
+    (let [ver (slurp in)]
+      (if *command-line-args*
+        (spit "/dev/stdout" ver)
+        (s/trim ver)))))
+
+(defn doc-title
+  "Returns the title of the document."
+  [file opts]
+  (msg "Retrieving document title ..." 1)
+  (with-open [file (io/reader file)]
+    (str (first (line-seq file)))))
+
 (defn install-resources
   "Installs the files required by the HTML file."
-  [res opts]
-  (msg "Installing resources ..." (:verb opts) 1)
-  (doseq [[path uris] (cp/resources (io/resource res))
-          :let [uri (first uris)
-                relative-path (subs path 1)
-                path (str res "/" relative-path)]]
-    (with-open [in (io/input-stream (io/resource path))]
-      (io/make-parents path)
-      (io/copy in (io/file path)))))
+  [opts]
+  (msg "Installing resources ..." 1 (:verb opts))
+  (let [res (io/resource "static")]
+    (when (fs/exists? res)
+      (doseq [[path uris] (cp/resources res)
+              :let [uri (first uris)
+                    relative-path (subs path 1)
+                    path (str res "/" relative-path)]]
+        (with-open [in (io/input-stream (io/resource path))]
+          (io/make-parents path)
+          (io/copy in (io/file path)))))))
 
 (defn wrap
   "Wraps TEXT with HTML necessary for correct page display."
   [opts args text]
   (let [[lead & _] args
         title (or (or (:title opts) (:titlehead opts))
-                  (doc-title lead))
-        header (or (:header opts)
-                   (:titlehead opts))]
+                  (doc-title lead opts))
+        header (or (:header opts) (:titlehead opts))]
     (html
      [:html
       [:head
@@ -99,7 +127,7 @@
 (defn mdify
   "Converts Markdown inputs to HTML strings."
   [opts args]
-  (msg "Loading input files ..." (:verb opts) 1)
+  ;; (msg "Loading input files ..." 1 (:verb opts))
   (wrap opts args
         (apply str (map #(md-to-html-string (slurp %)) args))))
 
@@ -111,7 +139,7 @@
 (defn write-html
   "Writes the HTML file to disk."
   [opts args]
-  (msg "Writing output files ..." (:verb opts) 1)
+  (msg "Writing output files ..." 1 (:verb opts))
   (let [output (or (:output opts))]
     (with-open [out (io/output-stream output)]
       (spit out (mdify opts args)))))
@@ -119,14 +147,11 @@
 (defn stage
   "Sets up the environment for arguments F and G."
   [opts args f g]
-  (let [res "static"]
-    (if (:resonly opts)
-      (install-resources res opts)
-      (if (files-exist? args)
-        (do (or (:htmlonly opts)
-                (install-resources res opts))
-            (f))
-        (g)))))
+  (if (files-exist? args)
+    (do (or (:htmlonly opts)
+            (install-resources opts))
+        (f))
+    (g)))
 
 (defn encode
   "Converts Markdown inputs to HTML."
@@ -141,13 +166,21 @@
   [opts args errors summary]
   (stage opts args
          #(write-html opts args)
-         #(exit 1 (usage summary))))
+         #(doexit (display-usage summary)) 1))
+
+(defn min-args?
+  "Returns true if the minimum amount of command line input is met."
+  [opts args]
+  (> (count args) 0))
 
 (defn -main
   [& args]
   (let [{:keys [options arguments errors summary]}
         (parse-opts args cli-opts)]
     (cond
-      (:help options) (exit 0 (usage summary))
-      errors (exit 1 (error-msg errors)))
-    (launch options arguments errors summary)))
+      (:help options) (doexit #(display-usage summary))
+      (:version options) (doexit #(version))
+      (:resonly options) (doexit #(install-resources options))
+      errors (msgexit (error-msg errors) 1))
+    (and (min-args? options arguments)
+         (launch options arguments errors summary))))
