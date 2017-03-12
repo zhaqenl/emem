b4c42b86b68c40d4c4a695b1f9df51bfaa4487d9
Rommel M. Martinez <ebzzry@gmail.com>
2017-03-12 15:21:18 +0800

    Move base64 data to another module

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject emem "0.2.34-SNAPSHOT"
+(defproject emem "0.2.35-SNAPSHOT"
   :description "A trivial Markdown to HTML converter"
   :url "http://ebzzry.github.io/emem"
   :license {:name "Eclipse Public License"
